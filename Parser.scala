18aac9194099818bde159026a0dc7985a495babc
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2022-08-27 23:41:39 -0600

    Add files via upload
    Updated Parser To Function Correctly
    (Cannot Split All Cases Correctly Yet)

--- Parser.scala
+++ Parser.scala
@@ -2,6 +2,7 @@ package com.bc.helloworld
 
 import scala.collection.mutable.ListBuffer
 import scala.io.Source.fromFile
+import util.control.Breaks._
 
 class ParsedString(val string: String, val line: Int = -1, val column: Int = -1, file: String = "Unknown"):
     def print(): Unit = println(s"[$file:$line:$column] $string")
@@ -14,10 +15,10 @@ class Parser:
         try {
             var line = 1;
             for string <- source.getLines() do {
-                val row = string.split(' ')
+                val row = string.split(' ').toList
 
                 for i <- row do {
-                    wordBuffer += ParsedString(string, line, i.length, path)
+                    wordBuffer += ParsedString(i, line, i.length, path)
                 }
 
                 line += 1
