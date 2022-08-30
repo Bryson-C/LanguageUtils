9091a6a46b7a5b3a7255036e0bfe3ad42e205c01
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2022-08-30 16:23:47 -0600

    Add files via upload

--- Parser.scala
+++ Parser.scala
@@ -16,21 +16,73 @@ class Parser:
         try {
             var buffer = String()
             var lastChar: Char = ' '
-            var line = 0
+            var line = 1
             for i <- source.getLines() do
+                var index = 0
                 breakable {
                     for chr <- i.toCharArray do
-                        print(s"chr: '$chr', ")
+                        if index > i.length then
+                            break
+
+                        val nextChar: Option[Char] =
+                            if index < i.length-1 then
+                                Some(i.charAt(index + 1))
+                            else
+                                None
+
+                        if chr == '/' && nextChar.get == '/' then
+                            break
+
+
+                        else if chr == '\"' then
+                            index += 1
+                            wordBuffer += ParsedString(buffer, line, index-buffer.length, path)
+                            buffer = ""
+                            while i.charAt(index) != '\"' do
+                                buffer += i.charAt(index)
+                                index += 1
+
+                            index += 1
+
+                            println(s"String '$buffer'")
+                            wordBuffer += ParsedString(buffer, line, index-buffer.length, path)
+                            buffer = ""
+
+                        else if chr.isDigit then
+                            wordBuffer += ParsedString(buffer, line, index-buffer.length, path)
+                            buffer = ""
+                            while chr.isDigit do
+                                println(s"$buffer")
+                                buffer += i.charAt(index)
+                                index += 1
+
+                            println(s"Integer '$buffer'")
+                            wordBuffer += ParsedString(buffer, line, index-buffer.length, path)
+                            buffer = ""
+
+                        else if chr == ' ' then
+                            wordBuffer += ParsedString(buffer, line, index-buffer.length, path)
+                            buffer = ""
+
+                        else if index == i.toCharArray.length then
+                            wordBuffer += ParsedString(buffer, line, index-buffer.length, path)
+                            buffer = ""
+
+
                         buffer += chr
                         lastChar = chr
+                        index += 1
+
+                    if buffer.nonEmpty then
+                        wordBuffer += ParsedString(buffer, line, index, path)
+                        buffer = ""
                 }
                 println()
-                wordBuffer += ParsedString(buffer, line)
-                buffer = ""
                 line += 1
 
 
         } catch {
+            case strErr: StringIndexOutOfBoundsException => println(s"String Index Error! '$strErr'")
             case x: Throwable => println(s"File Error Occurred! '$x'")
         } finally {
             source.close()
